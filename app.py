76fa6a69d061015fc153ed5ff1effafe84f7960b
Mauricio <mauricio96345@gmail.com>
2026-07-21 03:23:07 -0400

    ultimos ajustes de vistas

--- app.py
+++ app.py
@@ -25,17 +25,17 @@ def role_required(*roles):
 # @role_required("cdp")
 def index():
     # Require login
-    if "usuario" not in session:
-        return redirect(url_for("login"))
+    # if "usuario" not in session:
+    #     return redirect(url_for("login"))
 
-    connect=pymysql.connect(host="localhost",user="root",passwd="",database="serv_comunitario")
-    C=connect.cursor()
-    try:
+    # connect=pymysql.connect(host="localhost",user="root",passwd="",database="serv_comunitario")
+    # C=connect.cursor()
+    # try:
         usuario = session.get("usuario")
         rol = session.get("rol")
         return render_template('index.html', usuario=usuario, rol=rol, is_cdp=True)
-    finally:
-        connect.close()
+    # finally:
+        # connect.close()
 
 @app.route('/generar_reporte',methods=['GET','POST'])
 # @role_required("cdp")
@@ -65,8 +65,8 @@ def generar():
         print(val)
 
     # Require login for report generation view
-    if "usuario" not in session:
-        return redirect(url_for("login"))
+    # if "usuario" not in session:
+    #     return redirect(url_for("login"))
 
     usuario = session.get("usuario")
     rol = session.get("rol")
